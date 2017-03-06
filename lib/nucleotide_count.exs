f911c90c0bbea6addd6e9f167cf5a7e76c844ccb
GaizkaRubio <grubio@stratio.com>
2017-03-06 19:00:42 +0100

    Nucleotide count

--- lib/nucleotide_count.exs
+++ lib/nucleotide_count.exs
@@ -1,5 +1,34 @@
 defmodule NucleotideCount do
-  def count(dna, nucleo) do
-    1
+
+  def histogram('') do
+    %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+  end
+
+  def histogram(hist) do
+    %{?A => count(hist,?A), ?T => count(hist,?T), ?C => count(hist,?C), ?G => count(hist,?G)}
+  end
+
+  def count('', _) do
+    0
+  end
+
+  def count([?A|tail],?A) do
+    1 + count(tail, ?A)
+  end
+
+  def count([?C|tail],?C) do
+    1 + count(tail, ?C)
+  end
+
+  def count([?T|tail],?T) do
+      1 + count(tail, ?T)
+  end
+
+  def count([?G|tail],?G) do
+    1 + count(tail, ?G)
+  end
+
+  def count([head|tail], nucleo) do
+    count(tail, nucleo)
   end
 end
